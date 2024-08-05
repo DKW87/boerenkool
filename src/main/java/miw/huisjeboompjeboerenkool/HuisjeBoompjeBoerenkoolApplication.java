b5288c1cc665a49d5878ae8a1a0665224515b44c
Bart Notelaers <bart.notelaers@hva.nl>
2024-08-05 13:38:47 +0200

    initial empty project

---
+++ src/main/java/miw/huisjeboompjeboerenkool/HuisjeBoompjeBoerenkoolApplication.java
@@ -0,0 +1,13 @@
+package miw.huisjeboompjeboerenkool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuisjeBoompjeBoerenkoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuisjeBoompjeBoerenkoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/miw/huisjeboompjeboerenkool/HuisjeBoompjeBoerenkoolApplicationTests.java
@@ -0,0 +1,13 @@
+package miw.huisjeboompjeboerenkool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuisjeBoompjeBoerenkoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
