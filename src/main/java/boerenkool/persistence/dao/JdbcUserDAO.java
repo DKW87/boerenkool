7dc0137f00c4cbc7bed1b4d7aca930d8a60641e2
Leo Kurpershoek <leokurpershoek@hva.nl>
2024-08-07 17:28:19 +0200

    feat(user): add CR methods to UserRepository

--- src/main/java/boerenkool/persistence/dao/JdbcUserDAO.java
+++ src/main/java/boerenkool/persistence/dao/JdbcUserDAO.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Optional;
 
+@Repository
 public class JdbcUserDAO  implements UserDAO{
 
     private final Logger logger = LoggerFactory.getLogger(JdbcUserDAO.class);
