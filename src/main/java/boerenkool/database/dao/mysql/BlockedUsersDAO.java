b95702e4950785b8edcd6f6405617197b137fcf8
Leo Kurpershoek <leokurpershoek@hva.nl>
2024-08-08 15:31:53 +0200

    feat(blockedusers): add functionality  BlockedUsersDAO

--- src/main/java/boerenkool/database/dao/mysql/BlockedUsersDAO.java
+++ src/main/java/boerenkool/database/dao/mysql/BlockedUsersDAO.java
@@ -1,4 +1,23 @@
 package boerenkool.database.dao.mysql;
 
+import boerenkool.business.model.BlockedUsers;
+import boerenkool.business.model.User;
+import boerenkool.database.dao.GenericDAO;
+
+import java.util.List;
+
 public interface BlockedUsersDAO {
+    // Add a blocked user
+    void addBlockedUser(BlockedUsers blockedUsers);
+
+    // Remove a blocked user
+    void removeBlockedUser(BlockedUsers blockedUsers);
+
+    // Check if a user is blocked
+    boolean isUserBlocked(User blockedUser, User blockedByUser);
+
+    // Retrieve a list of users blocked by a specific user
+    List<User> getBlockedUsers(User blockedByUser);
+
+
 }
