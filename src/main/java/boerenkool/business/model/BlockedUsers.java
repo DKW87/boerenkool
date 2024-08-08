16a1995cc78f6bef1483022eb9b1bd39124c2d0c
Leo Kurpershoek <leokurpershoek@hva.nl>
2024-08-08 15:27:50 +0200

    feat(blockedusers): create BlockedUsers and add functionality

--- src/main/java/boerenkool/business/model/BlockedUsers.java
+++ src/main/java/boerenkool/business/model/BlockedUsers.java
@@ -2,6 +2,20 @@ package boerenkool.business.model;
 
 public class BlockedUsers {
 
+    private User blockedUser;
 
+    private User blockedByUser;
 
+    public BlockedUsers(User blockedUser, User blockedByUser) {
+        this.blockedUser = blockedUser;
+        this.blockedByUser = blockedByUser;
+    }
+
+    @Override
+    public String toString() {
+        return "BlockedUsers{" +
+                "blockedUser=" + blockedUser +
+                ", blockedByUser=" + blockedByUser +
+                '}';
+    }
 }
