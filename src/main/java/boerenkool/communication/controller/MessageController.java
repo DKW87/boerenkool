6070a38e494dfc65506ad866ee0e4e8ac98880ea
Bart Notelaers <bart.notelaers@hva.nl>
2024-08-09 14:31:40 +0200

    fix(#23) comment-out MessageController and MessageService to avoid compile errors

--- src/main/java/boerenkool/communication/controller/MessageController.java
+++ src/main/java/boerenkool/communication/controller/MessageController.java
@@ -1,18 +1,18 @@
-package boerenkool.communication.controller;
-
-import boerenkool.business.service.MessageService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController("/messages")
-public class MessageController {
-    private MessageService messageService;
-
-    @Autowired
-    public MessageController(MessageService messageService) {
-        this.messageService = messageService;
-    }
-
-}
+//package boerenkool.communication.controller;
+//
+//import boerenkool.business.service.MessageService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController("/messages")
+//public class MessageController {
+//    private MessageService messageService;
+//
+//    @Autowired
+//    public MessageController(MessageService messageService) {
+//        this.messageService = messageService;
+//    }
+//
+//}
