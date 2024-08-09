a01856c5b940d5cfc89d4cf627559baab19da61d
Leo Kurpershoek <leokurpershoek@hva.nl>
2024-08-09 11:41:54 +0200

    feat(user-tests): - added save_1 and save_ 2 tests -find_by_existing_id_test & find_by_non_existing_id_test

--- src/test/java/boerenkool/database/dao/mysql/JdbcUserDAOTests.java
+++ src/test/java/boerenkool/database/dao/mysql/JdbcUserDAOTests.java
@@ -1,6 +1,7 @@
 package boerenkool.database.dao.mysql;
 
 import boerenkool.business.model.User;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -51,4 +52,55 @@ public class JdbcUserDAOTests {
         assertThat(memberOption1).isPresent();
         assertThat(memberOption1.get().getUsername()).isEqualTo(leo.getUsername());
     }
+
+    @Test
+    void save_test_2() {
+        // This tests saving an existing member in the database;
+        // prerequisites: 1. the member alredy exists in the db
+        //                2. member.getId() > 0
+
+        // step 1: get an existing meber from the db:
+        Optional<User> user_option_1 = instanceUnderTest.getOneById(1);
+
+        // step 2: make sure that you have found a member_1:
+        Assertions.assertThat(user_option_1).isNotNull().isNotEmpty();
+
+        // step 3: remember its id, we will need that later:
+        User user_1 = user_option_1.get();
+        Assertions.assertThat(user_1.getUserId()).isGreaterThan(0);
+        int id = user_1.getUserId();
+
+        // step 4: modify the member_1:
+        user_1.setUsername("test_name");
+
+        // step 5: save the modified member_1:
+        instanceUnderTest.storeOne(user_1);
+
+        // step 6: assert that member_1.getId() has not changed:
+        Assertions.assertThat(user_1.getUserId()).isEqualTo(id);
+
+        // step 7: retrieve the member_1 from the datbase:
+        Optional<User> user_option_2 = instanceUnderTest.getOneById(id);
+
+        // step 8: verify that it is correct:
+        Assertions.assertThat(user_option_2).isNotNull().isNotEmpty();
+        User user_2 = user_option_2.get();
+        Assertions.assertThat(user_2).isNotNull().isEqualTo(user_1);
+    }
+
+    @Test
+    void find_by_existing_id_test() {
+        Optional<User> actual = instanceUnderTest.getOneById(1);
+        Assertions.assertThat(actual).isNotNull().isNotEmpty();
+        Assertions.assertThat(actual.get()).isNotNull().isEqualTo(leo);
+    }
+
+    @Test
+    void find_by_non_existing_id_test() {
+        Optional<User> actual = instanceUnderTest.getOneById(10000);
+        Assertions.assertThat(actual).isNotNull().isEmpty();
+    }
+
+
+
 }
