931b79666114599ce207272115dffce81faf819c
Bart Notelaers <bart.notelaers@hva.nl>
2024-08-09 14:36:33 +0200

    feat(#49) setup H2 testing database with create SQL script in schema.sql and demo test

--- src/test/java/boerenkool/HuisjeBoompjeBoerenkoolApplicationTests.java
+++ src/test/java/boerenkool/HuisjeBoompjeBoerenkoolApplicationTests.java
@@ -1,16 +1,23 @@
 package boerenkool;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @ActiveProfiles("test") // dit betekent dat de application-test.properties gebruikt moet worden
 class HuisjeBoompjeBoerenkoolApplicationTests {
 
+    @Autowired
+    HuisjeBoompjeBoerenkoolApplication application = null;
+
     @Test
-    void contextLoads() {
-        System.out.println("contextLoads");
+    void springBootApplicationContextLoadsOk() {
+        assertNotNull(application);
+        System.out.println("Application auto-configuration has succeeded.");
     }
 
 }
