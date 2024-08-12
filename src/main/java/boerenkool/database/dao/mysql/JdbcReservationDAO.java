3a7f8e68651a747519e874f17aaa10913302b9bb
adnan kilic <adnan.kilic@hva.nl>
2024-08-12 22:11:52 +0200

    aanpasingen bij reservation klassen na 1.review

--- src/main/java/boerenkool/database/dao/mysql/JdbcReservationDAO.java
+++ src/main/java/boerenkool/database/dao/mysql/JdbcReservationDAO.java
@@ -10,7 +10,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Repository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -26,37 +25,28 @@ import java.util.Optional;
  * @created 07/08/2024 - 20:53
  */
 
-@Repository
 public class JdbcReservationDAO implements ReservationDAO {
 
     private final Logger logger = LoggerFactory.getLogger(JdbcReservationDAO.class);
     private JdbcTemplate jdbcTemplate;
-    private final UserDAO userDAO;
-    private final HouseDAO houseDAO;
 
     @Autowired
     public JdbcReservationDAO(JdbcTemplate jdbcTemplate, UserDAO userDAO, HouseDAO houseDAO) {
         this.jdbcTemplate = jdbcTemplate;
-        this.userDAO = userDAO;
-        this.houseDAO = houseDAO;
         logger.info("New JdbcReservationDAO instance created.");
     }
 
     @Override
     public List<Reservation> getAll() {
         String sql = "select * from Reservation;";
-        return jdbcTemplate.query(sql, new ReservationMapper(userDAO, houseDAO));
+        return jdbcTemplate.query(sql, new ReservationMapper());
     }
 
     @Override
     public Optional<Reservation> getOneById(int id) {
         String sql = "select * from Reservation where reservationId = ?;";
-        List<Reservation> reservations = jdbcTemplate.query(sql, new ReservationMapper(userDAO, houseDAO), id);
-        if (reservations.isEmpty()) {
-            return Optional.empty();
-        } else {
-            return Optional.of(reservations.getFirst());
-        }
+        List<Reservation> reservations = jdbcTemplate.query(sql, new ReservationMapper(), id);
+        return reservations.stream().findFirst();
     }
 
     @Override
@@ -80,14 +70,18 @@ public class JdbcReservationDAO implements ReservationDAO {
         return rowsAffected > 0;
     }
 
-    private PreparedStatement insertReservationStatement(Reservation reservation, Connection connection) throws SQLException {
-        String sql = "INSERT INTO Reservation (reservedByUserId, houseId, startDate, endDate, guestCount) VALUES (?, ?, ?, ?, ?);";
-        PreparedStatement preparedStatement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+    private void extracted(Reservation reservation, PreparedStatement preparedStatement) throws SQLException {
         preparedStatement.setInt(1, reservation.getReservedByUser().getUserId());
         preparedStatement.setInt(2, reservation.getHouse().getHouseId());
         preparedStatement.setDate(3, java.sql.Date.valueOf(reservation.getStartDate()));
         preparedStatement.setDate(4, java.sql.Date.valueOf(reservation.getEndDate()));
         preparedStatement.setInt(5, reservation.getGuestCount());
+    }
+
+    private PreparedStatement insertReservationStatement(Reservation reservation, Connection connection) throws SQLException {
+        String sql = "INSERT INTO Reservation (reservedByUserId, houseId, startDate, endDate, guestCount) VALUES (?, ?, ?, ?, ?);";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+        extracted(reservation, preparedStatement);
         return preparedStatement;
     }
 
@@ -95,41 +89,35 @@ public class JdbcReservationDAO implements ReservationDAO {
         PreparedStatement preparedStatement;
         String sql = "UPDATE Reservation SET reservedByUserId=?, houseId=?, startDate=?, endDate=?, guestCount=? WHERE reservationId = ?";
         preparedStatement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
-        preparedStatement.setInt(1, reservation.getReservedByUser().getUserId());
-        preparedStatement.setInt(2, reservation.getHouse().getHouseId());
-        preparedStatement.setDate(3, java.sql.Date.valueOf(reservation.getStartDate()));
-        preparedStatement.setDate(4, java.sql.Date.valueOf(reservation.getEndDate()));
-        preparedStatement.setInt(5, reservation.getGuestCount());
+        extracted(reservation, preparedStatement);
         preparedStatement.setInt(6, reservation.getReservationId());
         return preparedStatement;
     }
 
     private static class ReservationMapper implements RowMapper<Reservation> {
 
-        private final UserDAO userDAO;
-        private final HouseDAO houseDAO;
-
-        public ReservationMapper(UserDAO userDAO, HouseDAO houseDAO) {
-            this.userDAO = userDAO;
-            this.houseDAO = houseDAO;
-        }
-
         @Override
         public Reservation mapRow(ResultSet resultSet, int rowNumber) throws SQLException {
             int reservationId = resultSet.getInt("reservationId");
             int reservedByUserId = resultSet.getInt("reservedByUserId");
-            User reservedByUser = userDAO.getOneById(reservedByUserId)
-                    .orElseThrow(() -> new SQLException("User not found with ID: " + reservedByUserId));
-
             int houseId = resultSet.getInt("houseId");
-            House house = houseDAO.getOneById(houseId)
-                    .orElseThrow(() -> new SQLException("House not found with ID: " + houseId));
-
             LocalDate startDate = resultSet.getDate("startDate").toLocalDate();
             LocalDate endDate = resultSet.getDate("endDate").toLocalDate();
             int guestCount = resultSet.getInt("guestCount");
 
-            return new Reservation(reservationId, house, reservedByUser, startDate, endDate, guestCount);
+            Reservation reservation = new Reservation();
+            reservation.setReservationId(reservationId);
+            reservation.setStartDate(startDate);
+            reservation.setEndDate(endDate);
+            reservation.setGuestCount(guestCount);
+
+            reservation.setHouse(new House()); //TODO: Een lege house constructor
+            reservation.getHouse().setHouseId(houseId);
+
+            reservation.setReservedByUser(new User());
+            reservation.getReservedByUser().setUserId(reservedByUserId);
+
+            return reservation;
         }
     }
 }
