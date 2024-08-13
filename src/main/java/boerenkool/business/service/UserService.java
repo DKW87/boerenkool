0f50d2ddeb3baefe4d9e3e628c224e8cf9a9fc79
Leo Kurpershoek <leokurpershoek@hva.nl>
2024-08-13 16:22:33 +0200

    fix(usercontroller) - vang fouten op van null values in service ipv controller

--- src/main/java/boerenkool/business/service/UserService.java
+++ src/main/java/boerenkool/business/service/UserService.java
@@ -2,6 +2,8 @@ package boerenkool.business.service;
 
 import boerenkool.business.model.User;
 import boerenkool.database.repository.UserRepository;
+import boerenkool.utilities.exceptions.UserNotFoundException;
+import boerenkool.utilities.exceptions.UserUpdateFailedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,24 +20,36 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void storeOne(User user) {userRepository.storeOne(user);}
+    public void storeOne(User user) {
+        userRepository.storeOne(user);
+    }
 
-    public boolean removeOneById(int id) {return userRepository.removeOneById(id);}
+    public boolean removeOneById(int id) {
+        boolean removed = userRepository.removeOneById(id);
+        if (!removed) {
+            throw new UserNotFoundException("User not found with id: " + id);
+        }
+        return true;
+    }
 
     public List<User> getAll() {
         return userRepository.getAll();
-    };//Read
+    }
 
-    public Optional getOneById(int id) {
+    public Optional<User> getOneById(int id) {
         return userRepository.getOneById(id);
     }
 
     public boolean updateOne(User user) {
-        return userRepository.updateOne(user);
+        boolean updated = userRepository.updateOne(user);
+        if (!updated) {
+            throw new UserUpdateFailedException("Failed to update user with id: " + user.getUserId());
+        }
+        return true;
     }
 
-    public Optional<User> findByUsername(String username) {
-        return userRepository.findByUsername(username);
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException("User not found with username: " + username));
     }
-
 }
