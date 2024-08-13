195031ca408a9b23f74cda8b21812e18e9e41f49
Leo Kurpershoek <leokurpershoek@hva.nl>
2024-08-13 15:27:03 +0200

    feat(authentication)

--- src/main/java/boerenkool/communication/controller/UserController.java
+++ src/main/java/boerenkool/communication/controller/UserController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "api/users")
 public class UserController {
 
     private final Logger logger = LoggerFactory.getLogger(UserController.class);
