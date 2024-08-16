1bdc81ed7e14bd656c9b13cc5b8377298f3230cd
Leo Kurpershoek <leokurpershoek@hva.nl>
2024-08-16 10:30:47 +0200

    feat(token) - add validationHandler method to RegistrationController

--- src/main/java/boerenkool/communication/controller/RegistrationController.java
+++ src/main/java/boerenkool/communication/controller/RegistrationController.java
@@ -4,22 +4,31 @@ import boerenkool.business.model.User;
 import boerenkool.business.service.RegistrationService;
 import boerenkool.communication.dto.LoginDTO;
 import boerenkool.communication.dto.UserDto;
+import boerenkool.utilities.authorization.AuthorizationService;
+import boerenkool.utilities.authorization.TokenUserPair;
+import boerenkool.utilities.exceptions.LoginException;
 import boerenkool.utilities.exceptions.RegistrationFailedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class RegistrationController {
 
     private final RegistrationService registrationService;
+    private final AuthorizationService authorizationService;
 
     @Autowired
-    public RegistrationController(RegistrationService registrationService) {
+    public RegistrationController(RegistrationService registrationService, AuthorizationService authorizationService) {
         this.registrationService = registrationService;
+        this.authorizationService = authorizationService;
     }
 
     @PostMapping("register")
@@ -29,15 +38,33 @@ public class RegistrationController {
     }
 
     @PostMapping("login")
-    public ResponseEntity<?> loginHandler(@RequestBody LoginDTO loginDTO) {
+    public ResponseEntity<UserDto> loginHandler(@RequestBody LoginDTO loginDTO) throws LoginException {
         User user = registrationService.validateLogin(
                 loginDTO.getUsername(), loginDTO.getPassword());
         if (user != null) {
-            // Token autorisatie (niet getoond hier)
-            return ResponseEntity.ok().body(new UserDto(user));
+            TokenUserPair tokenUserPair = authorizationService.authorize(user);
+            return ResponseEntity.ok()
+                    .header("Authorization", tokenUserPair.getKey().toString())
+                    .body(new UserDto(user));
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body("User does not exist or password is incorrect");
+            throw new LoginException("Login failed");
+
+        }
+    }
+
+
+    @PostMapping("validate")
+    public ResponseEntity<String> validationHandler(@RequestHeader String authorization) throws LoginException {
+        try {
+            UUID uuid = UUID.fromString(authorization);
+            Optional<User> user = authorizationService.validate(uuid);
+            if (user.isPresent()) {
+                return ResponseEntity.ok().body(user.get().getUsername());
+            } else {
+                throw new LoginException("Login failed");
+            }
+        } catch (IllegalArgumentException e) {
+            throw new LoginException("Login failed");
         }
     }
 }
