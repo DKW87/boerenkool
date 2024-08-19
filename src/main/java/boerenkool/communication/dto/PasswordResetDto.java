c599bbfa57688a21a69d0c9d12f8fd0e4bbd985e
Leo Kurpershoek <leokurpershoek@hva.nl>
2024-08-19 14:01:09 +0200

    feat(verification) - PasswordResetDto aangemaakt

---
+++ src/main/java/boerenkool/communication/dto/PasswordResetDto.java
@@ -0,0 +1,31 @@
+package boerenkool.communication.dto;
+
+public class PasswordResetDto {
+    private String email;
+    private String token;
+    private String newPassword;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
