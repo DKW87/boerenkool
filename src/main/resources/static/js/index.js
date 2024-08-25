eee1f702667bf3b15adad869cb655724ab2dbbb9
Danny Kwant <danny.kwant@hva.nl>
2024-08-25 09:20:53 +0200

    feat(front-end): init import VCS

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,95 @@
+"use strict"
+
+/* imports */
+
+
+/* load all page elements of index.html */
+loadHeader();
+loadLeftSidebar();
+loadBody();
+loadFooter();
+
+function loadHeader() {
+    const header = document.getElementById("header");
+    if (header) {
+        fetch('templates/header.html')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Netwerkreactie was niet ok.');
+                }
+                return response.text();
+            })
+            .then(data => {
+                header.innerHTML = data;
+            })
+            .catch(error => {
+                console.error('Er is een probleem opgetreden met fetch:', error);
+            });
+    } else {
+        console.error('Element met ID "header" niet gevonden.');
+    }
+}
+
+function loadLeftSidebar() {
+    const leftSidebar = document.getElementById("left-sidebar");
+    if (leftSidebar) {
+        fetch('templates/filter.html')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Netwerkreactie was niet ok.');
+                }
+                return response.text();
+            })
+            .then(data => {
+                leftSidebar.innerHTML = data;
+                initializePriceFilter();
+            })
+            .catch(error => {
+                console.error('Er is een probleem opgetreden met fetch:', error);
+            });
+    } else {
+        console.error('Element met ID "left-sidebar" niet gevonden.');
+    }
+}
+
+function loadBody() {
+    const body = document.getElementById("body");
+    if (body) {
+        fetch('templates/listOfHouses.html')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Netwerkreactie was niet ok.');
+                }
+                return response.text();
+            })
+            .then(data => {
+                body.innerHTML = data;
+            })
+            .catch(error => {
+                console.error('Er is een probleem opgetreden met fetch:', error);
+            });
+    } else {
+        console.error('Element met ID "body" niet gevonden.');
+    }
+}
+
+function loadFooter() {
+    const footer = document.getElementById("footer");
+    if (footer) {
+        fetch('templates/footer.html')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Netwerkreactie was niet ok.');
+                }
+                return response.text();
+            })
+            .then(data => {
+                footer.innerHTML = data;
+            })
+            .catch(error => {
+                console.error('Er is een probleem opgetreden met fetch:', error);
+            });
+    } else {
+        console.error('Element met ID "footer" niet gevonden.');
+    }
+}
