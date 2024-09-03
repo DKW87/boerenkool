0b5d05a1dc4a7315bd08caaf87b4be9f57157bc2
Leo Kurpershoek <leokurpershoek@hva.nl>
2024-09-03 16:43:55 +0200

    chore(blockeduserdto) add userid

--- src/main/java/boerenkool/communication/dto/BlockedUserDTO.java
+++ src/main/java/boerenkool/communication/dto/BlockedUserDTO.java
@@ -2,8 +2,10 @@ package boerenkool.communication.dto;
 
 public class BlockedUserDTO {
     private String username;
+    private int userId;
 
     public BlockedUserDTO(String username) {
+        this.userId = userId;
         this.username = username;
     }
 
