8a5c4356969d7e62b5001ed45f4219e6142e5b90
Leo Kurpershoek <leokurpershoek@hva.nl>
2024-09-03 13:28:53 +0200

    chore(profile) - remove eventlisten for update boerenkoolcoins

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -86,7 +86,6 @@ function configureUserSpecificSettings(user) {
 function setupEventListeners() {
     // Event listeners voor verschillende knoppen en formulierelementen
     document.getElementById('profileForm').addEventListener('submit', updateProfile);
-    document.getElementById('updateCoinsBtn').addEventListener('click', handleUpdateCoins);
     document.getElementById('deleteProfileBtn').addEventListener('click', deleteProfile);
     document.getElementById('block-user-btn').addEventListener('click', () => {
         const userId = document.getElementById('userId').value;
