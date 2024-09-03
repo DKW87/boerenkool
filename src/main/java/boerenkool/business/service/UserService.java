eff0da9d6c7a7db578e3cd55752bb8535a0100fc
Leo Kurpershoek <leokurpershoek@hva.nl>
2024-09-03 14:58:00 +0200

    fix(userservice) - return new ArrayList in getMapofCorrespondents

--- src/main/java/boerenkool/business/service/UserService.java
+++ src/main/java/boerenkool/business/service/UserService.java
@@ -8,6 +8,7 @@ import boerenkool.utilities.exceptions.UserUpdateFailedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -87,7 +88,7 @@ public class UserService {
     public List<Map<String, Object>> getMapOfCorrespondents(int userId) throws MessageDoesNotExistException {
         Optional<List<Map<String, Object>>> mapOfCorrespondents = userRepository.getMapOfCorrespondents(userId);
         if (mapOfCorrespondents.isEmpty()) {
-            throw new MessageDoesNotExistException();
+            return new ArrayList<>();
         } else {
             return mapOfCorrespondents.get();
         }
