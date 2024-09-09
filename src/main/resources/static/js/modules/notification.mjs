b2b65e4b5254ea7400ecad26ce302ea2d31aed2e
Leo Kurpershoek <leokurpershoek@hva.nl>
2024-09-09 10:13:24 +0200

    fix(notification) - remove shownotification

--- src/main/resources/static/js/modules/notification.mjs
+++ src/main/resources/static/js/modules/notification.mjs
@@ -1,18 +1,5 @@
 "use strict";
 
-export function showNotification(message, type) {
-    const notificationDiv = document.getElementById('notification');
-    if (notificationDiv) {
-        notificationDiv.textContent = message;
-        notificationDiv.className = type;
-        setTimeout(() => {
-            notificationDiv.textContent = '';
-            notificationDiv.className = '';
-        }, 5000);
-    } else {
-        console.error('Notification element not found');
-    }
-}
 
 export function showToast(message) {
     const toast = document.getElementById('toast');
