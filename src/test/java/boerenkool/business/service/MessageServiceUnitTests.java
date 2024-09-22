2c9d64dc6e604441a96e5720dfbfd9a106b616e0
Bart Notelaers <bart.notelaers@hva.nl>
2024-09-22 14:12:06 +0200

    style(tests) clean up Message tests

--- src/test/java/boerenkool/business/service/MessageServiceUnitTests.java
+++ src/test/java/boerenkool/business/service/MessageServiceUnitTests.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 
 
